97653da623f15a92d8ee8a7139cd1a330a88adaf
Michael Laufersweiler <michaellaufersweiler@Michaels-MacBook-Pro-2.local>
2024-03-28 09:33:38 -0500

    changing favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import { Merriweather } from "next/font/google";
 import "./globals.css";
 import Navbar from "../components/navbar/navbar";
 import localFont from "next/font/local";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Clare & Michael",
