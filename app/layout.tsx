889e3c6b00ed5cb7f77873d1d99283a97a08804e
Michael Laufersweiler <michaellaufersweiler@Michaels-MacBook-Pro-2.local>
2024-03-28 10:04:55 -0500

    still fixing favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,7 +31,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/favicon2.ico" />
       </Head>
       <body className={`${manstein.variable} ${ortical.variable}`}>
         <Navbar />
