362dcbc77ca5acf072f46f55bb67316b9619dc39
Michael Laufersweiler <michaellaufersweiler@Michaels-MBP-2.lan>
2024-04-25 08:17:11 -0400

    fixing name

--- lib/data.ts
+++ lib/data.ts
@@ -363,10 +363,10 @@ export const guests = [
     { id: 359, groupId: 174, name: "Reg McKillip", age: ADULT },
     { id: 360, groupId: 175, name: "John Schottmer", age: ADULT },
     { id: 361, groupId: 175, name: "Kellie Schottmer", age: ADULT },
-    { id: 362, groupId: 127, name: "Thomas Ricci", age: ADULT },
     { id: 363, groupId: 176, name: "Chad Kowalewski", age: ADULT },
     { id: 364, groupId: 177, name: "Siana Maradol", age: ADULT },
     { id: 365, groupId: 178, name: "Aaron Gonzalez", age: ADULT },
     { id: 366, groupId: 179, name: "Matt Reilly", age: ADULT },
     { id: 367, groupId: 179, name: "Maura Reilly", age: ADULT },
+    { id: 368, groupId: 127, name: "Thomas Ricci", age: ADULT },
 ];
